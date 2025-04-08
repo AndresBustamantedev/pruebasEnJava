b0bb9862371e48088eaeeff3f775a45b53fe9f8b
Andres Bustamante <jorgeandresbustamantetroya@gmail.com>
2025-04-09 00:18:22 +0200

    Base de juego del ahorcado con uso de bucles

--- Ahorcado/src/Ahorcado.java
+++ Ahorcado/src/Ahorcado.java
@@ -1,6 +1,5 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-import java.sql.SQLOutput;
 import java.util.Scanner;
 public class Ahorcado {
     public static void main(String[] args) {
